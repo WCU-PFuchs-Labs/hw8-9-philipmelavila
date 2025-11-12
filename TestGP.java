10d09599c00be8c5c3de39a9b0380b9ff15b027a
philipmelavila <3016242@wcupa.edu>
2025-11-11 20:55:27 -0500

    Add files via upload

---
+++ TestGP.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TestGP {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the name of the data file: ");
+        String fileName = scanner.nextLine();
+        Generation generation = new Generation(500, 3, fileName);
+        generation.evalAll();
+        
+        for (int generationNum = 1; generationNum <= 50; generationNum++) {
+            System.out.println("Generation " + generationNum + ":");
+            System.out.println("Best Tree: ");
+            generation.printBestTree();
+            System.out.print("Best Fitness: ");
+            generation.printBestFitness();
+            generation.evolve();
+        }
+        
+        scanner.close();
+    }
+}
